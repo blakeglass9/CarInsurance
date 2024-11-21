03fb3b9b1b8ddfe69d445647852a0190cff172e2
blakeglass9 <146895621+blakeglass9@users.noreply.github.com>
2024-11-21 13:48:07 -0800

    initial commit

---
+++ CarInsurance/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarInsurance.Models;
+
+namespace CarInsurance.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Insuree> Insurees { get; set; }
+    }
+}
